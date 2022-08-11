3685aa0f1298ba73c17b6c0ef18898bb7658967e
mteruel28 <miguel.teruel98@gmail.com>
2022-08-11 17:58:48 -0500

    New Commits

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+let submitBtn = document.getElementById("myButton")
+submitBtn.addEventListener('click', function() {
+    // navigation to beginner page
+    //let newpageURL = 'http://127.0.0.1:5500/MainMenu/Beginner.html'
+    //window.location.href = newpageURL
 
-document.getElementById("myButton").onclick = function(){
+    // input field element
+    let name =  document.getElementById("name").value
+    document.getElementById("myLabel").innerHTML = "Hello " + name + "!!!";
+})
 
-    var myName =document.getElementById ("myText").value;
-    console.log("Hello",myName);
-}
