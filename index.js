4f7de99a89ffdd8aaba1735c360b26128d215d18
mteruel28 <miguel.teruel98@gmail.com>
2022-08-12 01:14:45 -0500

    New Commits

--- index.js
+++ index.js
@@ -5,7 +5,26 @@ submitBtn.addEventListener('click', function() {
     //window.location.href = newpageURL
 
     // input field element
-    let name =  document.getElementById("name").value
+    let name =  document.getElementById("myname").value
     document.getElementById("myLabel").innerHTML = "Hello " + name + "!!!";
+
+    let experience = document.getElementById("myexperience").value
+    
+    if( (experience) <= 5 ) {
+        document.getElementById("myOutput").innerHTML = "I recommend beginner exercises ";
+    }  else if((experience) <= 7) {
+        document.getElementById("myOutput").innerHTML = "I recommend intermediate exercises ";
+    } else {
+        document.getElementById("myOutput").innerHTML = "I recommend advanced exercises ";
+  
+    }
+    
+       // var message;
+      //  var Exp = document.getElementById("myexperience").value;
+      //  if(Exp < 5){
+            //message = "I would like to recommend you the beginner exercises";
+           // document.getElementById("myexperience").innerHTML = message;
+
+   
 })
 
